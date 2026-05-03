23418e847185742a6d9881df16dd2e6e4aa3b3a9
thegreatcode1 <suhaibpa72@gmail.com>
2026-05-03 16:19:57 +0530

    app bar using count display

--- lib/custom_scroll_view_learn/sliverdashboard.dart
+++ lib/custom_scroll_view_learn/sliverdashboard.dart
@@ -30,7 +30,10 @@ class _SliverdashboardState extends State<Sliverdashboard> {
             flexibleSpace: FlexibleSpaceBar(
               centerTitle: false,
               background: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 12,
+                ),
                 child: Column(
                   mainAxisAlignment: .end,
                   children: [
@@ -63,7 +66,7 @@ class _SliverdashboardState extends State<Sliverdashboard> {
                   ],
                 ),
               ),
-              titlePadding: .symmetric(horizontal: 16),
+              titlePadding: .symmetric(horizontal: 16, vertical: 12),
               expandedTitleScale: 1.3,
               title: Text(
                 "Suhaib P A",
@@ -76,7 +79,234 @@ class _SliverdashboardState extends State<Sliverdashboard> {
             ),
           ),
 
-          SliverFillRemaining(),
+          //----------------------------------------//
+          SliverAppBar(
+            expandedHeight: 250,
+            backgroundColor: Colors.white60,
+
+            flexibleSpace: FlexibleSpaceBar(
+              stretchModes: [StretchMode.zoomBackground],
+              background: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 12,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: .spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.center,
+                            padding: .symmetric(horizontal: 16, vertical: 12),
+                            decoration: BoxDecoration(
+                              borderRadius: .circular(20),
+                              color: Colors.blue,
+                            ),
+
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: .end,
+                                  children: [
+                                    Text(
+                                      "Target",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 12,
+                                        fontWeight: .bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      "(14/20)",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 16,
+                                        fontWeight: .bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                Row(
+                                  mainAxisAlignment: .start,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "12",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 24,
+                                            fontWeight: .bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          "Daily",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 14,
+                                            fontWeight: .w500,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 12),
+                        // container 2
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.center,
+
+                            padding: .symmetric(horizontal: 16, vertical: 12),
+                            decoration: BoxDecoration(
+                              borderRadius: .circular(20),
+                              color: Colors.deepOrange,
+                            ),
+
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: .end,
+                                  children: [
+                                    Text(
+                                      "Target",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 12,
+                                        fontWeight: .bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      "(5/40)",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: .bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                Row(
+                                  mainAxisAlignment: .start,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "5",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 24,
+                                            fontWeight: .bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          "weekly",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: .w500,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            //width: double.infinity,
+
+                            //alignment: Alignment.center,
+                            padding: .symmetric(horizontal: 16, vertical: 12),
+                            decoration: BoxDecoration(
+                              borderRadius: .circular(20),
+                              color: Colors.limeAccent,
+                            ),
+
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: .end,
+                                  children: [
+                                    Text(
+                                      "Target",
+                                      style: TextStyle(
+                                        color: Colors.black54,
+                                        fontSize: 12,
+                                        fontWeight: .bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      "(10/60)",
+                                      style: TextStyle(
+                                        color: Colors.black54,
+                                        fontSize: 16,
+                                        fontWeight: .bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                Row(
+                                  mainAxisAlignment: .start,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "10",
+                                          style: TextStyle(
+                                            color: Colors.black54,
+                                            fontSize: 24,
+                                            fontWeight: .bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          "mothly",
+                                          style: TextStyle(
+                                            color: Colors.black54,
+                                            fontSize: 14,
+                                            fontWeight: .w500,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          SliverFillRemaining(hasScrollBody: true),
         ],
       ),
     );
